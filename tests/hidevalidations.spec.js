4bfae66bed381691e83f26934c104adeea588b30
bilal-sakhawat-de <bilal.sakhawat.de@gmail.com>
2025-09-04 03:13:42 +0200

    Added a new spec.js test file for hide elements on the web page.

---
+++ tests/hidevalidations.spec.js
@@ -0,0 +1,18 @@
+    const {test,expect} = require("@playwright/test");
+     
+     
+    test("Hide Elements Validation",async({page})=>
+    {
+        await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+        await page.goto("https://www.google.com/");
+        await page.goBack();
+
+        await expect(page.locator("#displayed-text").isVisible());
+        await page.locator("#hide-textbox").click();
+        await expect(page.locator("#displayed-text").isHidden());
+     
+          
+
+     
+     
+    })
