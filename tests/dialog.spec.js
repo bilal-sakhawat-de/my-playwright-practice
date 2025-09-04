220ce315cbae5646e8c993688e66b77375de5e35
bilal-sakhawat-de <bilal.sakhawat.de@gmail.com>
2025-09-04 03:39:34 +0200

    Added a dialog accept/dismiss test file.

---
+++ tests/dialog.spec.js
@@ -0,0 +1,12 @@
+    const {test,expect} = require("@playwright/test");
+     
+     
+    test("Dialog Confirm & Dismiss", async({page}) =>
+    {
+        await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+        await page.locator("#confirmbtn").click();
+        page.on('dialog', async dialog => {
+            dialog.accept();
+        })
+     
+    })
